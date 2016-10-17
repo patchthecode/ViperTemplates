5db2f17945ea2ad0aec846a518098a7204417690
JayT <patchthecode@gmail.com>
2016-10-16 19:52:34 -0700

    first commit

---
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Models.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import UIKit
+
+struct ___FILEBASENAMEASIDENTIFIER___ {
+    struct ___VARIABLE_sceneTask___ {
+        struct Request {
+            
+        }
+        
+        struct Response {
+            
+        }
+        
+        struct ViewModel {
+            
+        }
+    }
+}
---
+++ Clean Swift/Scene.xctemplate/UIViewController/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,30 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//
+
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___PresenterInput {
+  func presentSomething(response: ___FILEBASENAMEASIDENTIFIER___.___VARIABLE_sceneTask___.Response)
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___PresenterOutput: class {
+  func displaySomething(viewModel: ___FILEBASENAMEASIDENTIFIER___.___VARIABLE_sceneTask___.ViewModel)
+}
+
+class ___FILEBASENAMEASIDENTIFIER___Presenter: ___FILEBASENAMEASIDENTIFIER___PresenterInput {
+    weak var output: ___FILEBASENAMEASIDENTIFIER___PresenterOutput!
+  
+    // MARK: Presentation logic
+    func presentSomething(response: ___FILEBASENAMEASIDENTIFIER___.___VARIABLE_sceneTask___.Response) {
+        // NOTE: Format the response from the Interactor and pass the result back to the View Controller
+
+        let viewModel = ___FILEBASENAMEASIDENTIFIER___.___VARIABLE_sceneTask___.ViewModel()
+        output.displaySomething(viewModel: viewModel)
+    }
+}
